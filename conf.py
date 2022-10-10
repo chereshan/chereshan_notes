121b182d3257d598ef4fa5ec0e4223d5d103e5ce
chereshan <55876010+chereshan@users.noreply.github.com>
2022-10-11 01:21:41 +0700

    trouble shoottings in regard to nbsphinx

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["nbsphinx"]
+extensions = ["nbsphinx",]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
